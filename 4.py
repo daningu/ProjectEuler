0944c86147f640e7fe85bdc42fa5db118e233034
Unknown <gnubag@gmail.com>
2018-04-17 18:08:46 -0700

    4 Solved
    Problem 4 solved

--- 4.py
+++ 4.py
@@ -1,7 +1,21 @@
-def lendigit(n):
-	return len(str(n))
+# check if forward == backward
+def checkpalindrome(n):
+	flag = True
+	digits = []
+	digits = [int(x) for x in str(n)]
+	for i in range(len(digits)):
+		if digits[i] != digits[len(digits)-1-i]:
+			return False
+	return True
+
+
 # generate products
+pal = [0]
 for i in range(999,0,-1):
 	for j in range(999,0,-1):
 		prod = i*j
-# check if 
+		if checkpalindrome(prod) == True:
+			if prod > pal[0]:
+				pal[0] = prod
+print pal
+
